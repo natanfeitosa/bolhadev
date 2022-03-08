be6743212eafcddbc6d2c8ae0fb526e29d87ed29
Natanael dos Santos Feitosa <52074821+natanfeitosa@users.noreply.github.com>
2022-03-08 07:05:27 +0000

    fix: port let

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const {
 
 require('./tweet').initiate(io)
 
-let port = process.env.PORT || 3000
+let PORT = process.env.PORT || 3000
 server.listen(PORT, () => {
   console.log('listing on port:', PORT)
 })
